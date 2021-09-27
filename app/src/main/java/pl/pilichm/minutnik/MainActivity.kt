9c1f84834ace3d5c8d25f75056341582b4152853
pilichm <michal.pilichowski@onet.pl>
2021-09-27 20:26:08 +0200

    Added: - initial code logic for starting, stopping and restarting timer.

--- app/src/main/java/pl/pilichm/minutnik/MainActivity.kt
+++ app/src/main/java/pl/pilichm/minutnik/MainActivity.kt
@@ -1,11 +1,72 @@
 package pl.pilichm.minutnik
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var mSelectedTime: Int = 0
+    private var isCounterRunning = false
+    private var mCountDownTimer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpSeekBar()
+        setUpStartStopButton()
+    }
+
+    private fun setUpSeekBar(){
+        selectTimeSeekBar.progress = MAX_TIMER_VALUE_IN_SECONDS/2
+        selectTimeSeekBar.max = MAX_TIMER_VALUE_IN_SECONDS
+        mSelectedTime = MAX_TIMER_VALUE_IN_SECONDS/2
+        tvTimeLeft.text = "$mSelectedTime ${resources.getString(R.string.seconds)}"
+
+        selectTimeSeekBar.setOnSeekBarChangeListener(
+            object: SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                mSelectedTime = progress
+                tvTimeLeft.text = "$mSelectedTime ${resources.getString(R.string.seconds)}"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+
+    private fun setUpStartStopButton(){
+        btnStopStart.setOnClickListener {
+            if (isCounterRunning){
+                btnStopStart.text = resources.getString(R.string.button_start)
+                mCountDownTimer!!.cancel()
+                isCounterRunning = false
+            } else {
+                btnStopStart.text = resources.getString(R.string.button_stop)
+                mCountDownTimer = object : CountDownTimer(
+                    mSelectedTime * TIMER_TICK_PERIOD, TIMER_TICK_PERIOD) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        val mTImeRemaining = (millisUntilFinished/ TIMER_TICK_PERIOD).toInt()
+                        selectTimeSeekBar.progress = mTImeRemaining
+                        mSelectedTime = mTImeRemaining
+                        tvTimeLeft.text = "${millisUntilFinished/ TIMER_TICK_PERIOD} ${resources.getString(R.string.seconds)}"
+                    }
+
+                    override fun onFinish() {
+                        isCounterRunning = false
+                    }
+                }.start()
+                isCounterRunning = true
+            }
+        }
+    }
+
+    companion object {
+        const val MAX_TIMER_VALUE_IN_SECONDS = 120
+        const val TIMER_TICK_PERIOD = 1000L
     }
 }
